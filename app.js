71e4a6a2c79c66a59f3d4cb8e0f61e9354b990b3
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-11-23 21:49:24 +0600

    Laptop data API created using expressJS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const laptops = require('./laptops.json')
 const app = express()
 const port = 3000
 
@@ -6,6 +7,18 @@ app.get('/', (req, res) => {
   res.send('My Laptop-dataaa')
 })
 
+app.get('/laptops', (req, res) => {
+    res.send(laptops);
+})
+
+app.get('/laptops/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const laptop = laptops.find(laptop => laptop.id === id)
+
+    res.send(laptop);
+
+})
 app.listen(port, () => {
   console.log(`My laptop data listening on port ${port}`)
 })
